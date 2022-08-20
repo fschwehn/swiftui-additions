98531cd6f9259ea00e3f4d2dce889871273e9937
fschwehn <fschwehn@gmail.com>
2022-08-21 00:25:10 +0200

    fix iOS build

--- Package.swift
+++ Package.swift
@@ -13,16 +13,16 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "melatonin",
+            name: "melatonin-dynamic",
+            type: .dynamic,
+            targets: ["melatonin"]),
+        .library(
+            name: "melatonin-static",
+            type: .static,
             targets: ["melatonin"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "melatonin",
             dependencies: []),
